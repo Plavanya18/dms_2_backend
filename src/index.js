6f3219c815a3adf3664b32f477f87f91244b8938
Plavanya18 <prakashlavanya18@gmail.com>
2026-01-06 12:07:46 +0530

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-const app = require('./app');
+// const app = require('./app');
+// const dotenv = require('dotenv');
+// const logger = require('./config/logger');
+// dotenv.config();
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   logger.info(`Listening to port for ${PORT}`);
+// });
 const dotenv = require('dotenv');
+dotenv.config(); // LOAD ENV FIRST
+
+const app = require('./app');
 const logger = require('./config/logger');
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 

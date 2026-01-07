b640da892b04d2f802d59f1a7ecde184d357e766
Plavanya18 <prakashlavanya18@gmail.com>
2026-01-07 11:41:21 +0530

    sliced the path in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -44,8 +44,19 @@ const publicPaths = [
   '/auth/reset-password',
 ];
 
+// router.use((req, res, next) => {
+//   const path = req.path.replace(/\/$/, "");
+
+//   if (publicPaths.includes(path)) {
+//     return next();
+//   }
+
+//   return verifyToken(req, res, next);
+// });
 router.use((req, res, next) => {
-  const path = req.path.replace(/\/$/, "");
+  let path = req.originalUrl.replace(/\/$/, "");
+  if (path.startsWith('/api')) {
+  }
 
   if (publicPaths.includes(path)) {
     return next();

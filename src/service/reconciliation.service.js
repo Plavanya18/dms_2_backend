74d6428a8d013af23803554ea6607d544e9c0302
Plavanya18 <prakashlavanya18@gmail.com>
2026-03-27 12:12:35 +0530

    reconciliation of adding tzs in opening and closing

--- src/service/reconciliation.service.js
+++ src/service/reconciliation.service.js
@@ -690,7 +690,7 @@ const getAllReconciliations = async ({
         else if (o.currency.code === "TZS") openingTZS += Number(o.amount || 0);
       });
 
-      const totalOpeningValue = openingUSD * openingRate;
+      const totalOpeningValue = openingUSD * openingRate + openingTZS;
 
       let closingUSD = 0, closingTZS = 0;
       rec.closingEntries.forEach(c => {
@@ -698,7 +698,11 @@ const getAllReconciliations = async ({
         else if (c.currency.code === "TZS") closingTZS += Number(c.amount || 0);
       });
 
-      const totalClosingValue = closingUSD * closingRate;
+      console.log("closingRate", closingRate)
+      console.log("closingUSD", closingUSD)
+      console.log("closingTZS", closingTZS)
+      
+      const totalClosingValue = closingUSD * closingRate + closingTZS;
 
       const totalValueOut = totalTzsPaid + (totalForeignSold * closingRate);
       const totalValueIn = totalTzsReceived + (totalForeignBought * closingRate);
